4982b6bd1947eb6bcc7ce331e3e8ebc0516a2377
kiikoh <ikikomonkey@gmail.com>
2023-08-06 16:56:40 -0400

    add strict status codes

--- apps/gadgets-service/src/contract.ts
+++ apps/gadgets-service/src/contract.ts
@@ -9,30 +9,35 @@ const GadgetSchema = z.object({
   body: z.string(),
 });
 
-export const contract = c.router({
-  createGadget: {
-    method: "POST",
-    path: "/gadgets",
-    summary: "Create a gadget",
-    description: "Create a gadget",
-    responses: {
-      201: GadgetSchema,
-      400: z.object({
-        message: z.string(),
+export const contract = c.router(
+  {
+    createGadget: {
+      method: "POST",
+      path: "/gadgets",
+      summary: "Create a gadget",
+      description: "Create a gadget",
+      responses: {
+        201: GadgetSchema,
+        400: z.object({
+          message: z.string(),
+        }),
+      },
+      body: z.object({
+        title: z.string(),
+        body: z.string(),
       }),
     },
-    body: z.object({
-      title: z.string(),
-      body: z.string(),
-    }),
-  },
-  getGadget: {
-    method: "GET",
-    path: `/gadgets/:id`,
-    summary: "Create a gadget",
-    description: "Get a gadget by id",
-    responses: {
-      200: GadgetSchema.nullable(),
+    getGadget: {
+      method: "GET",
+      path: `/gadgets/:id`,
+      summary: "Create a gadget",
+      description: "Get a gadget by id",
+      responses: {
+        200: GadgetSchema.nullable(),
+      },
     },
   },
-});
+  {
+    strictStatusCodes: true,
+  }
+);
--- apps/widgets-service/src/contract.ts
+++ apps/widgets-service/src/contract.ts
@@ -9,30 +9,35 @@ const PostSchema = z.object({
   body: z.string(),
 });
 
-export const contract = c.router({
-  createWidget: {
-    method: "POST",
-    path: "/widgets",
-    summary: "Create a widget",
-    description: "Create a widget",
-    responses: {
-      201: PostSchema,
-      400: z.object({
-        message: z.string(),
+export const contract = c.router(
+  {
+    createWidget: {
+      method: "POST",
+      path: "/widgets",
+      summary: "Create a widget",
+      description: "Create a widget",
+      responses: {
+        201: PostSchema,
+        400: z.object({
+          message: z.string(),
+        }),
+      },
+      body: z.object({
+        title: z.string(),
+        body: z.string(),
       }),
     },
-    body: z.object({
-      title: z.string(),
-      body: z.string(),
-    }),
-  },
-  getWidget: {
-    method: "GET",
-    path: `/widgets/:id`,
-    summary: "Create a widget",
-    description: "Get a widget by id",
-    responses: {
-      200: PostSchema.nullable(),
+    getWidget: {
+      method: "GET",
+      path: `/widgets/:id`,
+      summary: "Create a widget",
+      description: "Get a widget by id",
+      responses: {
+        200: PostSchema.nullable(),
+      },
     },
   },
-});
+  {
+    strictStatusCodes: true,
+  }
+);
